faf0c526cebbec7bac8ecff4979af5b8b999f9a6
Giri2002 <girisadhasivam@gmail.com>
2024-11-04 17:34:43 +0530

    image

---
+++ home.js
@@ -0,0 +1,24 @@
+// // let currentSlide = 0;
+
+// // function showSlide(index) {
+// //     const slides = document.querySelectorAll('.slider-container');
+// //     slides.forEach((slide, i) => {
+// //         slide.classList.remove('active');
+// //         if (i === index) {
+// //             slide.classList.add('active');
+// //         }
+// //     });
+// // }
+
+// let currentIndex = 0;
+
+// function changeSlide(direction) {
+//     const slides = document.querySelectorAll('.slider-container .image-wrapper');
+//     slides[currentIndex].style.display = 'none'; // Hide current slide
+//     currentIndex = (currentIndex + direction + slides.length) % slides.length; // Update index
+//     slides[currentIndex].style.display = 'block'; // Show new slide
+// }
+
+// // Optional: Auto slide functionality
+// setInterval(() => changeSlide(1), 5000); // Change slide every 5 seconds
+
